50ef4aa990248efe46f2b3933847e11452478c0d
myy <545825@qq.com>
2016-08-15 18:25:23 +0800

    test

---
+++ Shop/Common/Conf/db.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'DB_TYPE' => 'mysql', // 数据库类型
+	'DB_HOST' => 'localhost', // 数据库服务器地址
+	'DB_NAME' => 'myshop', // 数据库名称
+	'DB_USER' => 'root', // 数据库用户名
+	'DB_PWD' => 'myy545', // 数据库密码
+	'DB_PORT' => '3306', // 数据库端口
+	'DB_PREFIX' => 'sh_', // 数据表前缀
+	'AUTH' => 'jk$jk8f889*ds#fk&jkj', // 数据加密密钥
+);
